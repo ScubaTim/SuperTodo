3f99bfb3b812f226ff35938578e17ec6adbf524a
Tim Gentry <gentry.tim@gmail.com>
2021-05-30 20:07:29 -0400

    added joi validation and schema

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,9 +1,20 @@
 import { Todo } from '../models/todo.js'
 import express from 'express'
+import Joi from 'joi'
 
 const router = express.Router()
 
 router.post('/', async (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(200).required(),
+        author: Joi.string().min(3).max(30),
+        uid: Joi.string(),
+        isComplete: Joi.boolean(),
+        date: Joi.date()
+    })
+
+    schema.validate(req.body)
+
     const { name, author, uid, isComplete, date } = req.body
 
     let todo = new Todo({
