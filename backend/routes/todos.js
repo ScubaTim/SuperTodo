5be7d02d562bacfe19415e6520ab92f8a416edfc
Tim Gentry <gentry.tim@gmail.com>
2021-06-01 12:40:23 -0400

    put endpoint added

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -50,6 +50,43 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put("/:id", async (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(200).required(),
+        author: Joi.string().min(3).max(30),
+        uid: Joi.string(),
+        isComplete: Joi.boolean(),
+        date: Joi.date()
+    })
+
+    const { error } = schema.validate(req.body)
+
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+
+    const todo = await Todo.findById(req.params.id)
+
+    if (!todo) {
+        console.log("Todo not found")
+        return res.status(404).send("Todo not found.")
+    }
+
+    const { name, author, uid, isComplete, date } = req.body
+
+    try {
+        const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, { name, author, uid, isComplete, date }, { new: true })
+
+        res.send(updatedTodo)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+        console.log(err.message)
+    }
+
+
+})
+
 router.delete("/:id", async (req, res) => {
     try {
         const deletedTodo = await Todo.findByIdAndDelete(req.params.id)
