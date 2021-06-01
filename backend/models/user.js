a127e8e4c84dab1cb76b536cc15f27f93b54f4aa
Tim Gentry <gentry.tim@gmail.com>
2021-06-01 16:02:05 -0400

    backend crud operations done and added user model

---
+++ backend/models/user.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 200,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 1024
+    }
+})
+
+export const User = mongoose.model('User', userSchema)
